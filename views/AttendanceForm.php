24697b9ade8b7e793329250023fca170e2beeb27
unknown <kristianelmerdelatorre@gmail.com>
2026-05-29 10:10:25 +0800

    Created a view file for Feedback and Attendance form

---
+++ views/AttendanceForm.php
@@ -0,0 +1,1 @@
+<!-- Joshua bunyad na here -->
---
+++ views/FeedbackForm.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <p>Logged in as: ... </p>
+        <label for="">Event Name:</label>
+        <select>
+            <option value="">Suntukan sa ace hardware</option>
+            <option value="">Magkamot sa makati</option>
+        </select>
+        <label for="star-rating">Star rating or sum typa shi</label>
+        <input type="radio" name="" id="star-rating">⭐
+        <input type="radio" name="" id="star-rating">⭐⭐
+        <input type="radio" name="" id="star-rating">⭐⭐⭐
+        <input type="radio" name="" id="star-rating">⭐⭐⭐⭐
+        <input type="radio" name="" id="star-rating">⭐⭐⭐⭐⭐
+
+        <label for="">Comment</label>
+        <textarea name="" id=""></textarea>
+    </form>
+</body>
+</html>
